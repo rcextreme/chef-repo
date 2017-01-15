8c2a77216071b320afeeb89368c7551fe747cfd5
rcextreme <rcalderon9p@gmail.com>
2017-01-15 20:51:23 +0000

    first commit

---
+++ cookbooks/myapp/metadata.rb
@@ -0,0 +1,19 @@
+name 'myapp'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures myapp'
+long_description 'Installs/Configures myapp'
+version '0.1.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/myapp/issues' if respond_to?(:issues_url)
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/myapp' if respond_to?(:source_url)
---
+++ cookbooks/myapp/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: myapp
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
